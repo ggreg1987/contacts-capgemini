48e781bfa23a16411b8d126005dee7bd8ad9e789
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 08:59:22 -0300

    Creating the endpoint create

--- src/main/java/one/digitalinnovation/contact/domain/rest/PhoneController.java
+++ src/main/java/one/digitalinnovation/contact/domain/rest/PhoneController.java
@@ -1,11 +1,15 @@
 package one.digitalinnovation.contact.domain.rest;
 
 import lombok.RequiredArgsConstructor;
+import one.digitalinnovation.contact.domain.entities.Phone;
 import one.digitalinnovation.contact.domain.repository.PhoneRepository;
+import one.digitalinnovation.contact.domain.rest.dto.PhoneDTO;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("api/phones")
@@ -15,4 +19,12 @@ public class PhoneController {
     private final PhoneRepository repository;
     private final PhoneService service;
     private final ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public PhoneDTO create(@RequestBody PhoneDTO dto) {
+        Phone phone = modelMapper.map(dto, Phone.class);
+        Phone save = service.save(phone);
+        return modelMapper.map(save, PhoneDTO.class);
+    }
 }
