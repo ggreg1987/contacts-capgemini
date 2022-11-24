1323165eb4558f0b4e07a3a5b2f708ace5309ec7
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 13:08:34 -0300

    Creating a static method to reuse

--- src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
+++ src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
@@ -1,5 +1,8 @@
 package one.digitalinnovation.contact.service;
 
+import one.digitalinnovation.contact.domain.entities.Person;
+import one.digitalinnovation.contact.domain.entities.Phone;
+import one.digitalinnovation.contact.domain.enums.PhoneType;
 import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
 import one.digitalinnovation.contact.domain.rest.services.impl.PhoneServiceImpl;
@@ -21,5 +24,21 @@ public class PhoneServiceTest {
     @BeforeEach
     public void setUp() {
         this.phoneService = new PhoneServiceImpl(phoneRepository);
+    }
+
+    public static Phone createPhone() {
+        Person person = Person
+                .builder()
+                .cpf("68029446004")
+                .build();
+
+        return Phone
+                .builder()
+                .id(1L)
+                .number("12345")
+                .type(PhoneType.MOBILE)
+                .person(person)
+                .build();
+
     }
 }
