486dade3850b4c6717e42b2641aa2100d09af1e0
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 07:13:58 -0300

    Starting the Project

---
+++ src/main/java/one/digitalinnovation/contact/ContactApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/contact/ContactApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
