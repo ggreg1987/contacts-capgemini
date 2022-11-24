208afa43cb10e261b5914c8d7fde26514a128599
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 17:39:31 -0300

    Testing savePersonTest - OK

--- src/test/java/one/digitalinnovation/contact/service/PersonServiceTest.java
+++ src/test/java/one/digitalinnovation/contact/service/PersonServiceTest.java
@@ -1,16 +1,24 @@
 package one.digitalinnovation.contact.service;
 
+import one.digitalinnovation.contact.domain.entities.Person;
 import one.digitalinnovation.contact.domain.repository.PersonRepository;
-import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.services.PersonService;
-import one.digitalinnovation.contact.domain.rest.services.PhoneService;
 import one.digitalinnovation.contact.domain.rest.services.impl.PersonServiceImpl;
-import one.digitalinnovation.contact.domain.rest.services.impl.PhoneServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -24,6 +32,27 @@ public class PersonServiceTest {
     @BeforeEach
     public void setUp() {
         this.personService = new PersonServiceImpl(personRepository);
+    }
+
+    private Person createPerson() {
+        return Person
+                .builder()
+                .cpf("68029446004")
+                .name("Gabriel Gregorio")
+                .email("gabriel@gmail.com")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Should save a Person")
+    public void savePersonTest() {
+        Person person = createPerson();
+
+        when(personRepository.save(Mockito.any())).thenReturn(person);
+
+        Person personTest = personService.save(person);
+
+        assertThat(personTest.getCpf()).isEqualTo(person.getCpf());
     }
 
 }
