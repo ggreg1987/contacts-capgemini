9c21ad3243b8bc2ca22239c04e658f0608a83aa7
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 12:15:35 -0300

    Creating findById method

--- src/main/java/one/digitalinnovation/contact/domain/rest/services/PersonService.java
+++ src/main/java/one/digitalinnovation/contact/domain/rest/services/PersonService.java
@@ -2,7 +2,10 @@ package one.digitalinnovation.contact.domain.rest.services;
 
 import one.digitalinnovation.contact.domain.entities.Person;
 
+import java.util.Optional;
+
 public interface PersonService {
 
     Person save(Person person);
+    Optional<Person> findById(String cpf);
 }
--- src/main/java/one/digitalinnovation/contact/domain/rest/services/impl/PersonServiceImpl.java
+++ src/main/java/one/digitalinnovation/contact/domain/rest/services/impl/PersonServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +26,10 @@ public class PersonServiceImpl implements PersonService {
                 .format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
         return repository.save(person);
+    }
+
+    @Override
+    public Optional<Person> findById(String cpf) {
+        return repository.findByCpf(cpf);
     }
 }
