2a8f3ff33e6ddc9909c919fcd03252b7b2a1d6a4
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 09:26:12 -0300

    Creating save method in PersonServiceImpl

---
+++ src/main/java/one/digitalinnovation/contact/domain/rest/services/PersonService.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.contact.domain.rest.services;
+
+import one.digitalinnovation.contact.domain.entities.Person;
+
+public interface PersonService {
+
+    Person save(Person person);
+}
---
+++ src/main/java/one/digitalinnovation/contact/domain/rest/services/impl/PersonServiceImpl.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.contact.domain.rest.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import one.digitalinnovation.contact.domain.entities.Person;
+import one.digitalinnovation.contact.domain.repository.PersonRepository;
+import one.digitalinnovation.contact.domain.rest.services.PersonService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PersonServiceImpl implements PersonService {
+
+    private final PersonRepository repository;
+
+    @Override
+    public Person save(Person person) {
+        if(repository.existsByCpf(person.getCpf())) {
+            throw new RuntimeException("Duplicated CPF");
+        }
+        return repository.save(person);
+    }
+}
