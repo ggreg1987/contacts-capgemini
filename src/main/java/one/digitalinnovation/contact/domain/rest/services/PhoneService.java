f0069552e8069fc5dc3cf566dc701cad6ed675b9
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 07:50:54 -0300

    Creating a method to findAll

--- src/main/java/one/digitalinnovation/contact/domain/rest/services/PhoneService.java
+++ src/main/java/one/digitalinnovation/contact/domain/rest/services/PhoneService.java
@@ -1,6 +1,8 @@
 package one.digitalinnovation.contact.domain.rest.services;
 
 import one.digitalinnovation.contact.domain.entities.Phone;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -8,4 +10,6 @@ public interface PhoneService {
 
     Phone save(Phone phone);
     Optional<Phone> findById(Long id);
+
+    Page<Phone> find(Phone phone, Pageable pageable);
 }
--- src/main/java/one/digitalinnovation/contact/domain/rest/services/impl/PhoneServiceImpl.java
+++ src/main/java/one/digitalinnovation/contact/domain/rest/services/impl/PhoneServiceImpl.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import one.digitalinnovation.contact.domain.entities.Phone;
 import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -22,5 +26,20 @@ public class PhoneServiceImpl implements PhoneService {
     @Override
     public Optional<Phone> findById(Long id) {
         return repository.findById(id);
+    }
+
+    @Override
+    public Page<Phone> find(Phone phone, Pageable pageable) {
+
+        ExampleMatcher exampleMatcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(phone,exampleMatcher);
+
+        return repository.findAll(example,pageable);
+
+
     }
 }
