9e2e5d9979c09096230d709998dd95626ccfc7fa
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 13:04:14 -0300

    Creating a new instance before starting tests

--- src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
+++ src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
@@ -2,6 +2,8 @@ package one.digitalinnovation.contact.service;
 
 import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
+import one.digitalinnovation.contact.domain.rest.services.impl.PhoneServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,4 +17,9 @@ public class PhoneServiceTest {
 
     @MockBean
     PhoneRepository phoneRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.phoneService = new PhoneServiceImpl(phoneRepository);
+    }
 }
