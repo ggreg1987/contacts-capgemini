a3074a2e2ee323ef1c635979774731cb5dbaa8ea
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 09:08:53 -0300

    Creating the endpoint findAll

--- src/main/java/one/digitalinnovation/contact/domain/rest/PhoneController.java
+++ src/main/java/one/digitalinnovation/contact/domain/rest/PhoneController.java
@@ -6,9 +6,15 @@ import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.dto.PhoneDTO;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -34,5 +40,15 @@ public class PhoneController {
         return service.findById(id)
                 .map(phone -> modelMapper.map(phone, PhoneDTO.class))
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+    }
+    @GetMapping
+    public Page<PhoneDTO> find(PhoneDTO dto, Pageable pageable) {
+        Phone phone = modelMapper.map(dto, Phone.class);
+        Page<Phone> phones = service.find(phone, pageable);
+        List<PhoneDTO> dtoList = phones.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, PhoneDTO.class))
+                .collect(Collectors.toList());
+        return new PageImpl<>(dtoList,pageable,phones.getTotalElements());
     }
 }
