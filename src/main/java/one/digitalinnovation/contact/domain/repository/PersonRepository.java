b0827fd023f9e8b0a5a94badd833484544c1a105
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 07:37:21 -0300

    Creating PersonRepository

---
+++ src/main/java/one/digitalinnovation/contact/domain/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.contact.domain.repository;
+
+import one.digitalinnovation.contact.domain.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface PersonRepository extends JpaRepository<Person,String> {
+
+    Optional<Person> findByCpf(String cpf);
+}
