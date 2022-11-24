4b97f9baebedf9801688690bc95ad89ec8f29cdc
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-24 13:23:13 -0300

    Testing save phone - OK

--- src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
+++ src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
@@ -7,10 +7,15 @@ import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
 import one.digitalinnovation.contact.domain.rest.services.impl.PhoneServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -39,6 +44,30 @@ public class PhoneServiceTest {
                 .type(PhoneType.MOBILE)
                 .person(person)
                 .build();
+
+    }
+    @Test
+    @DisplayName("Should save a phone")
+    public void savePhone() {
+
+        Phone phone = createPhone();
+        Person person = createPhone().getPerson();
+        Phone phoneReturned = Phone
+                    .builder()
+                    .id(1L)
+                    .number("12345")
+                    .type(PhoneType.MOBILE)
+                    .person(person)
+                    .build();
+
+        when(phoneRepository.save(phone)).thenReturn(phoneReturned);
+
+        Phone phoneTest = phoneService.save(phone);
+
+        assertThat(phoneTest.getId()).isEqualTo(phoneReturned.getId());
+        assertThat(phoneTest.getNumber()).isEqualTo(phoneReturned.getNumber());
+        assertThat(phoneTest.getPerson().getCpf()).isEqualTo(phoneReturned.getPerson().getCpf());
+        assertThat(phoneTest.getType().name()).isEqualTo(phoneReturned.getType().name());
 
     }
 }
