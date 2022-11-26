c21d7406209d858692521c8a041b6e6d3917f65f
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-26 20:38:36 -0300

    Testig find by id service

--- src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
+++ src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
@@ -6,17 +6,24 @@ import one.digitalinnovation.contact.domain.enums.PhoneType;
 import one.digitalinnovation.contact.domain.repository.PhoneRepository;
 import one.digitalinnovation.contact.domain.rest.services.PhoneService;
 import one.digitalinnovation.contact.domain.rest.services.impl.PhoneServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Assertions.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.Optional;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
@@ -53,6 +60,7 @@ public class PhoneServiceTest {
     }
 
     @Test
+    @DisplayName("Should create a Phone.")
     public void createPhoneTest() {
         Long id = 1L;
         Phone phone = createNewPhone();
@@ -73,6 +81,21 @@ public class PhoneServiceTest {
         assertThat(saved.getType().name()).isEqualTo(phone.getType().name());
         assertThat(saved.getNumber()).isEqualTo(phone.getNumber());
         assertThat(saved.getPerson().getCpf()).isEqualTo(phone.getPerson().getCpf());
+    }
+
+    @Test
+    @DisplayName("Should find phone by id")
+    public void findByIdTest() {
+        Long id = 1L;
+        Phone phone = createNewPhone();
+        phone.setId(id);
+
+        when(repository.findById(id)).thenReturn(Optional.of(phone));
+
+        Phone serviceById = service.findById(id);
+
+        assertThat(serviceById.getNumber()).isEqualTo(phone.getNumber());
+        assertThat(serviceById.getType().name()).isEqualTo(phone.getType().name());
     }
 
 }
