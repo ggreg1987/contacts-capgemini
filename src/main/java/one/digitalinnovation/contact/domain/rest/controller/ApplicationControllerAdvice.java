d16184da30db93e937fcc312a2ea4226ae19eeb0
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-26 19:59:10 -0300

    Creating a Class to show the errors,exceptionHandlers

---
+++ src/main/java/one/digitalinnovation/contact/domain/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.contact.domain.rest.controller;
+
+import one.digitalinnovation.contact.exceptions.ApiErrors;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus
+    public ApiErrors handleNotFoundException(ResponseStatusException ex) {
+        String message = ex.getMessage();
+        return new ApiErrors(message);
+    }
+    
+}
