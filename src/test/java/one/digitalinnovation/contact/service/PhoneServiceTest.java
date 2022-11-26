4433519eb5169f92e81f1c4ac40743818e11a239
Gabriel <gabriel.gregorio1987@gmail.com>
2022-11-26 17:16:58 -0300

    Creating a PhoneServiceTest class

---
+++ src/test/java/one/digitalinnovation/contact/service/PhoneServiceTest.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.contact.service;
+
+import one.digitalinnovation.contact.domain.repository.PhoneRepository;
+import one.digitalinnovation.contact.domain.rest.services.PhoneService;
+import one.digitalinnovation.contact.domain.rest.services.impl.PhoneServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+public class PhoneServiceTest {
+
+    PhoneService service;
+
+    @MockBean
+    PhoneRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        service = new PhoneServiceImpl(repository);
+    }
+
+}
